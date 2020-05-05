6fb35802c318d9197f646a380e026ba215e77497
Itay Porezky <Itay.Porezky@gmail.com>
2020-05-06 00:34:02 +0300

    fixed gitignore and added connection to spotify api

---
+++ SpotifyToYoutube/SpotifyToYoutube.SpotifyData/Connect.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SpotifyToYoutube.SpotifyData
+{
+    public class Connect
+    {
+
+        public static void Main(string[] args)
+        {
+            
+        }
+        public static async Task MainAsync()
+        {
+            
+            Console.WriteLine("Connecting to Spotify API");
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("https://accounts.spotify.com");
+                    var content = new FormUrlEncodedContent(new[]
+                    {
+
+                    new KeyValuePair<string, string>("grant_type", "client_credentials")
+                    });
+
+                    //content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                    client.DefaultRequestHeaders.Add("Authorization", "Basic ");
+
+                    HttpResponseMessage response = await client.PostAsync("/api/token", content);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+
+    }
+}
---
+++ SpotifyToYoutube/SpotifyToYoutube/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using SpotifyToYoutube.SpotifyData;
+
+namespace SpotifyToYoutube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Task.Run(() => Connect.MainAsync());
+            Console.ReadLine();
+        }
+    }
+}
